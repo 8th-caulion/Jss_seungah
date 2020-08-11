caede8e6f5f5028b1ff65e36b2c46ae39337e427
SA_21 <safg0421@cau.ac.kr>
2020-08-11 14:06:29 +0900

    8th lecture:comment

--- JssProject/main/models.py
+++ JssProject/main/models.py
@@ -8,3 +8,9 @@ class Jasoseol(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 # User : 자소서 = 1 : N
 # on_delete=models.CASCADE 연결된 오브젝트가 지워지면 연결된 자소서 오브젝트들도 사라진다.
+
+class Comment(models.Model):
+    content = models.CharField(max_length=100)
+    author = models.ForeignKey(User,on_delete=models.CASCADE)
+    jasoseol = models.ForeignKey(Jasoseol, on_delete=models.CASCADE)
+    # 자소설보다 밑에 있어야 인식이됌!
